1ee622c2fe27075935aad44064c1fc352a4d9545
Soumi7 <soumibardhan10@gmail.com>
2021-09-07 15:40:40 +0530

    Add docker

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def studenteditlink(iname):
       return render_template('editstudent.html',sname=iname,  obj=name_var)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host="0.0.0.0",port=80)
